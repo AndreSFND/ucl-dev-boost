0c96aae59b63cdc9155e3d5d85af758e7179195f
Mauwmau <maurilio.meireles@gmail.com>
2019-09-11 17:05:36 -0300

    ]'

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ function Header(){
 
   return (
 
-    <header class="bg-red-500 h-screen w-screen">
+    <header class="bg-blue-300 h-screen w-screen">
 			<div class="flex flex-col justify-center w-2/3 h-full mx-auto text-center">
         <div class="flex flex-row justify-center w-full">
           <img src={logo} alt="Logo"/>
