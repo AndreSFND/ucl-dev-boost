d47554a54fc6380488f92a891a310b44d189253d
AndreSFND <andre_sfnd@hotmail.com>
2019-09-11 17:01:54 -0300

    teste

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 module.exports = {
   theme: {
-    fontFamily: {
-      'subtitle': ['Arial', 'Helvetica', 'Futura']
-    },
-    colors: {
-      ucl: '#5ec8ae'
-    },
-    extend: {}
-  },
-  variants: {},
-  plugins: []
+    extend: {
+      colors: {
+        'ucl': '#5ec8ae',
+      }
+    }
+  }
 }
