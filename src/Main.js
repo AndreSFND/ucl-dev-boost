c62717ac266c190c07f173018028444c23dac027
Mauwmau <maurilio.meireles@gmail.com>
2019-09-18 17:42:37 -0300

    main-recent-flex

--- src/Main.js
+++ src/Main.js
@@ -4,22 +4,21 @@ function Main(){
 
   return (
 
-    <main class="bg-gray-100 w-screen h-auto">
+    <main className="bg-gray-100 w-screen h-auto">
 
-      <div class="flex flex-col justify-center w-full h-full text-center">
+      <div className="flex flex-col justify-center w-full h-full text-center">
       
-        <h1 class="text-6xl mt-8 font-merriweather font-semibold text-gray-900">Playlists mais Vistas</h1>
+        <h1 className="text-6xl mt-8 font-merriweather font-semibold text-gray-900">Playlists mais Vistas</h1>
 
-        <PainelTop title="HTML" />
-        <PainelTop title="JS" />
+        <PainelTop title="Mais Recentes" />
 
       </div>
 
-      <div class="flex flex-col justify-center w-full h-full text-center">
+      <div className="flex flex-col justify-center w-full h-full text-center">
 
-        <h1 class="text-6xl font-merriweather font-semibold text-gray-900">Outras</h1>
+        <h1 className="text-6xl font-merriweather font-semibold text-gray-900">Outras</h1>
 
-        <div class="flex flex-col w-2/3 mx-auto my-10 text-left bg-white border rounded shadow-md">
+        <div className="flex flex-col w-2/3 mx-auto my-10 text-left bg-white border rounded shadow-md">
 
           <PainelOutros />
           <PainelOutros />
@@ -38,31 +37,17 @@ function PainelTop(props){
 
   return (
 
-    <div class="flex flex-col w-2/3 mx-auto my-10 text-left bg-white border rounded shadow-md">
+    <div className="flex flex-col w-2/3 mx-auto my-10 text-left bg-white border rounded shadow-md">
 
-      <h1 class="text-2xl m-2 ml-3 font-montserrat text-gray-800 uppercase">{props.title}</h1>
+      <h1 className="text-2xl m-2 ml-3 font-montserrat text-gray-800 uppercase">{props.title}</h1>
 
-      <div class="flex flex-row w-full h-full border-t">
+      <div className="flex flex-col w-full h-full border-t p-6 items-stretch justify-around">
 
-        <div class="flex flex-row justify-center items-center w-5/6 h-48">
-        
+          <SubPainelTop videoname="My Puppies Video" description="Puppy placeholder foreeeeeveeeerr" />
           <SubPainelTop />
           <SubPainelTop />
           <SubPainelTop />
           <SubPainelTop />
-          <SubPainelTop />
-        
-        </div>
-
-        <div class="flex flex-row justify-center items-center w-1/6 h-48">
-    
-            <a href="https://www.youtube.com" target="blank">
-              <button class="border-2 border-blue-500 hover:border-blue-600 bg-transparent text-blue-600 hover:text-blue-700 py-3 px-5 font-semibold rounded font-montserrat">
-                Ver mais
-              </button>
-            </a>
-          
-          </div>
 
       </div>
 
@@ -74,10 +59,23 @@ function PainelTop(props){
 
 function SubPainelTop(props){
 
-  return (
+    /** @namespace props.videoname */
+    /** @namespace props.description */
 
-    <div class="max-w-sm shadow-lg h-auto mx-8">
-      <img class="w-40" src={require('./res/img/place.png')} alt="Puppy" />
+    return (
+
+    <div className="flex flex-row shadow-lg border border-gray-300 rounded-lg h-auto w-auto mx-2 my-4 p-2">
+        <div>
+            <img className="w-40" src={require('./res/img/place.png')} alt="Puppy"/>
+        </div>
+        <div className="flex flex-col align-top items-stretch w-full ml-6">
+            <h1 className="text-2xl font-medium mb-2 border-b border-gray-300">
+                {props.videoname || "Video indiponível"}
+            </h1>
+
+            <p className="font-hairline text-lg"> {props.description || "Descrição indisponivel"} </p>
+        </div>
+
     </div>
 
   );
@@ -88,7 +86,7 @@ function PainelOutros(){
 
   return (
 
-    <div class="flex flex-row w-full h-full justify-center items-center p-10">
+    <div className="flex flex-row w-full h-full justify-center items-center p-10">
 
       <SubPainelOutros />
       <SubPainelOutros />
@@ -105,8 +103,8 @@ function SubPainelOutros(){
 
   return (
 
-    <div class="max-w-sm shadow-lg h-36 mx-8">
-      <img class="w-full" src={require('./res/img/place2.png')} alt="Puppy" />
+    <div className="max-w-sm shadow-lg h-36 mx-8">
+      <img className="w-full" src={require('./res/img/place2.png')} alt="Puppy" />
     </div>
 
   );
--- src/Nav.js
+++ src/Nav.js
@@ -3,21 +3,21 @@ import React from 'react';
 function Nav(){
 
   return (
-    <nav class="fixed bg-ucl h-24 w-screen shadow">
-        <div class="flex flex-row w-full h-full border">
+    <nav className="fixed bg-ucl h-24 w-screen shadow">
+        <div className="flex flex-row w-full h-full border">
 
-          <div class="w-1/6 h-full border flex items-center justify-center">
-            <h1 class="text-4xl font-merriweather font-semibold text-white cursor-pointer">Dev.academy()</h1>
+          <div className="w-1/6 h-full border flex items-center justify-center">
+            <h1 className="text-4xl font-merriweather font-semibold text-white cursor-pointer">Dev.academy()</h1>
           </div>
 
-          <div class="w-3/6 h-full border">
+          <div className="w-3/6 h-full border">
             
           </div>
 
-          <div class="w-2/6 h-full border flex items-center justify-around">
-            <h1 class="text-xl font-montserrat font-semibold text-white cursor-pointer">Início</h1>
-            <h1 class="text-xl font-montserrat font-semibold text-white cursor-pointer">Entrar</h1>
-            <button class="border-2 border-white-500 hover:border-white-600 bg-transparent text-white py-3 px-5 font-semibold rounded font-montserrat text-xl">
+          <div className="w-2/6 h-full border flex items-center justify-around">
+            <h1 className="text-xl font-montserrat font-semibold text-white cursor-pointer">Início</h1>
+            <h1 className="text-xl font-montserrat font-semibold text-white cursor-pointer">Entrar</h1>
+            <button className="border-2 border-white-500 hover:border-white-600 bg-transparent text-white py-3 px-5 font-semibold rounded font-montserrat text-xl">
               Cadastrar
             </button>
           </div>
