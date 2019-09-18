c12156d60b66b1c57adf71acfb708f2f0505b7a8
Andre Santana Fernandes <USUARIOS+11208537@6-303-09.grad.usricmc.icmc.usp.br>
2019-09-18 16:32:16 -0300

    naosei

--- src/Nav.js
+++ src/Nav.js
@@ -7,16 +7,20 @@ function Nav(){
     <nav class="fixed bg-ucl h-24 w-screen shadow">
         <div class="flex flex-row w-full h-full border">
 
-          <div class="w-1/6 h-full border text-center">
-            <h1 class="text-4xl font-merriweather font-semibold text-white">Dev.academy()</h1>
+          <div class="w-1/6 h-full border flex items-center justify-center">
+            <h1 class="text-4xl font-merriweather font-semibold text-white cursor-pointer">Dev.academy()</h1>
           </div>
 
           <div class="w-3/6 h-full border">
             
           </div>
 
-          <div class="w-2/6 h-full border">
-            
+          <div class="w-2/6 h-full border flex items-center justify-around">
+            <h1 class="text-xl font-montserrat font-semibold text-white cursor-pointer">Início</h1>
+            <h1 class="text-xl font-montserrat font-semibold text-white cursor-pointer">Entrar</h1>
+            <button class="border-2 border-white-500 hover:border-white-600 bg-transparent text-white py-3 px-5 font-semibold rounded font-montserrat text-xl">
+              Cadastrar
+            </button>
           </div>
 
         </div>
