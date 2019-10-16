eb8cbe0e7c03ba53bd0504740205b68aec83df74
Andre Santana Fernandes <USUARIOS+11208537@6-303-06.grad.usricmc.icmc.usp.br>
2019-10-16 18:14:41 -0300

    Inicio do back

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,20 +1,35 @@
 const functions = require('firebase-functions');
 const express   = require('express');
+const admin     = require('firebase-admin');
+const cors      = require('cors');
+const bp        = require('body-parser');
 const app       = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+app.use(cors());
+app.use(bp.json());
 
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//     response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+    credential: admin.credential.cert("./firebase-keys.json"),
+    databaseURL: "https://ucl-dev-boost.firebaseio.com/"
+});
+
+const database = admin.database();
+const ref = database.ref("/ola");
 
 app.get('/', function (req, res) {
-    res.send('Got a GET request');
+    // const videoId = req.headers.videoId;
+    // database.ref("/videos/" + videoId).once("value", snapshot => {
+    //     res.status(200).send(snapshot.val());
+    // });
 });
 
 app.post('/', function (req, res) {
-    res.send('Got a POST request');
+    const url = JSON.parse(req.body).videoURL;
+    if(url === undefined) res.status(400).send("Must send and URL");
+    database.ref("/videos").push(url).then((videoRef)=>{
+        res.status(200).send(videoRef.key);
+    });
+    // res.send(`<iframe width="560" height="315" src="${url}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
 });
 
 app.put('/', function (req, res) {
